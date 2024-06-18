060549df77c8583a663ca10c8b26d843856fd654
kelechi kingsley <kelechigbe@yahoo.com>
2024-06-18 14:08:16 +0100

    Add BaseQuestion class and QuestionTypes enum

---
+++ CapitalPlacement.TalentManagement/CapitalPlacement.TalentManagement.Domain/BaseQuestion.cs
@@ -0,0 +1,15 @@
+﻿using CapitalPlacement.TalentManagement.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalPlacement.TalentManagement.Domain
+{
+    public abstract class BaseQuestion
+    {
+        public QuestionType Type { get; set; }
+        public string? Question { get; set; }
+    }
+}
---
+++ CapitalPlacement.TalentManagement/CapitalPlacement.TalentManagement.Domain/Enums/QuestionType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalPlacement.TalentManagement.Domain.Enums
+{
+    public enum QuestionType
+    {
+        [Description("Paragraph")]
+        Paragraph = 1,
+        [Description("Short answer")]
+        ShortAnswer = 2,
+        [Description("Yes/No")]
+        YesNo = 3,
+        [Description("Drop down")]
+        DropDown = 4,
+        [Description("Multiple choice")]
+        MultipleChoice = 5,
+        [Description("Date")]
+        Date = 6,
+        [Description("Number")]
+        Number = 7,
+        [Description("FileUpload")]
+        FileUpload = 8,
+        [Description("Video question")]
+        VideoQuestion = 9,
+
+    }
+}
