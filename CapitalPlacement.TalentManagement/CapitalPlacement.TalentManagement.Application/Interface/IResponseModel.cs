f4f6600797a4bbfe919acb86a967a4f70277966c
kelechi kingsley <kelechigbe@yahoo.com>
2024-06-18 15:44:43 +0100

    Add response model helper

---
+++ CapitalPlacement.TalentManagement/CapitalPlacement.TalentManagement.Application/Interface/IResponseModel.cs
@@ -0,0 +1,24 @@
+﻿
+namespace CapitalPlacement.TalentManagement.Application.Interface
+{
+    /// <summary>
+    /// Represents the response model interface.
+    /// </summary>
+    public interface IResponseModel
+    {
+        /// <summary>
+        /// Gets or sets the exception.
+        /// </summary>
+        Exception Ex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the message.
+        /// </summary>
+        string Message { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the operation was successful.
+        /// </summary>
+        bool IsSuccess { get; set; }
+    }
+}
