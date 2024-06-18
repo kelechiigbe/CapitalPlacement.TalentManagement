81a5ba96b4bde2b0857b4b0ec024360f580645a8
kelechi kingsley <kelechigbe@yahoo.com>
2024-06-18 15:57:24 +0100

    Add createprogramviewmodel and createprogramviewmodel validator

---
+++ CapitalPlacement.TalentManagement/CapitalPlacement.TalentManagement.Application/FluentValidators/CreateProgramViewModelValidator.cs
@@ -0,0 +1,19 @@
+﻿using CapitalPlacement.TalentManagement.Application.ViewModel;
+using FluentValidation;
+namespace CapitalPlacement.TalentManagement.Application.FluentValidators
+{
+    /// <summary>
+    /// Validator for the CreateProgramViewModel class.
+    /// </summary>
+    public class CreateProgramViewModelValidator : AbstractValidator<CreateProgramViewModel>
+    {
+        /// <summary>
+        /// Validates the Title and Description properties.
+        /// </summary>
+        public CreateProgramViewModelValidator()
+        {
+            RuleFor(program => program.Title).NotEmpty();
+            RuleFor(program => program.Description).NotEmpty();
+        }
+    }
+}
