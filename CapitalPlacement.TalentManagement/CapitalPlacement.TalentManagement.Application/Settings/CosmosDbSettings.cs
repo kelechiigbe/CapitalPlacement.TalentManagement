33c336893ad83e9c083446b26ecde4e43c9fde16
kelechi kingsley <kelechigbe@yahoo.com>
2024-06-18 17:08:36 +0100

    Add cosmo db settings

---
+++ CapitalPlacement.TalentManagement/CapitalPlacement.TalentManagement.Application/Settings/CosmosDbSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalPlacement.TalentManagement.Application.Settings
+{
+    public class CosmosDbSettings
+    {
+        public string AccountEndPoint { get; set; }
+        public string AccountKey { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
