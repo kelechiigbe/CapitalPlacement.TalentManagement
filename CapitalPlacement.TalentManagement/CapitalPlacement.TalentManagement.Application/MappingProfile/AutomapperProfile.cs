5614a60eb33576727b22a5f3662b1e6cd4bb2888
kelechi kingsley <kelechigbe@yahoo.com>
2024-06-18 17:45:18 +0100

    add mapping profile

---
+++ CapitalPlacement.TalentManagement/CapitalPlacement.TalentManagement.Application/MappingProfile/AutomapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CapitalPlacement.TalentManagement.Application.ViewModel;
+using CapitalPlacement.TalentManagement.Domain;
+
+namespace CapitalPlacement.TalentManagement.Application.MappingProfile
+{
+    public class AutomapperProfile: Profile
+    {
+        public AutomapperProfile() { 
+
+            CreateMap<CreateProgramRequest, Program>().ReverseMap();
+
+        }
+    }
+}
