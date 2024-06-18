f53f2c4e380b67be34a384fcc549c23366162de1
kelechi kingsley <kelechigbe@yahoo.com>
2024-06-18 14:29:25 +0100

    Add dropdown and multichoice templates

---
+++ CapitalPlacement.TalentManagement/CapitalPlacement.TalentManagement.Domain/DropdownsTemplate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalPlacement.TalentManagement.Domain
+{
+    public class DropdownsTemplate: BaseQuestion
+    {
+        public DropdownsTemplate() {
+            Type = Enums.QuestionType.DropDown;
+        }
+
+        public HashSet<string> Choice { get; set; }
+
+        public bool EnableOtherOption { get; set; }
+    }
+}
---
+++ CapitalPlacement.TalentManagement/CapitalPlacement.TalentManagement.Domain/MultiChoiceQuestionTemplate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalPlacement.TalentManagement.Domain
+{
+    public class MultiChoiceQuestionTemplate: DropdownsTemplate
+    {
+        public MultiChoiceQuestionTemplate() {
+
+            Type = Enums.QuestionType.MultipleChoice;
+        }
+
+        public int MaxChoiceAllowed { get; set; }
+    }
+}
