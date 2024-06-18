9a1e3ae99f9aceff01844df7b360e5033cd975c8
kelechi kingsley <kelechigbe@yahoo.com>
2024-06-18 16:02:21 +0100

    Register FuluentValidator service

---
+++ CapitalPlacement.TalentManagement/CapitalPlacement.TalentManagement.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using CapitalPlacement.TalentManagement.Application.FluentValidators;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CapitalPlacement.TalentManagement.Application.Extensions
+{
+    /// <summary>
+    /// Extensions for configuring application services.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds application services to the service collection.
+        /// </summary>
+        /// <param name="services">The current service collection.</param>
+        /// <returns>The updated service collection.</returns>
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services.AddFluentValidationAutoValidation()
+                          .AddValidatorsFromAssemblyContaining<CreateProgramViewModelValidator>();
+        }
+    }
+}
