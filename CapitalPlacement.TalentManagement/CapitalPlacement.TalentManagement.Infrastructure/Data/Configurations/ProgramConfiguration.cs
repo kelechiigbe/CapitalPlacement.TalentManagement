55e6a7710b3f3fecdf3f625dfd49dbad4d357c2d
kelechi kingsley <kelechigbe@yahoo.com>
2024-06-18 16:39:58 +0100

    Add program configuration

---
+++ CapitalPlacement.TalentManagement/CapitalPlacement.TalentManagement.Infrastructure/Data/Configurations/ProgramConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapitalPlacement.TalentManagement.Domain;
+
+namespace CapitalPlacement.TalentManagement.Infrastructure.Data.Configurations
+{
+    /// <summary>
+    /// Configuration class for the Program entity.
+    /// </summary>
+    public class ProgramConfiguration : IEntityTypeConfiguration<Program>
+    {
+        /// <summary>
+        /// Configures the properties of the Program entity.
+        /// </summary>
+        /// <param name="builder">Entity type builder for Author.</param>
+        public void Configure(EntityTypeBuilder<Program> builder)
+        {
+            builder.Property(p => p.Title)
+                   .IsRequired();
+            builder.Property(p => p.Description)
+                  .IsRequired();
+        }
+    }
+}
